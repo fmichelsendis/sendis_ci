d83f666c8285b822f5bd59c0a0078f1fb9cabdf3
fmichelsendis <franco.michelsendis@gmail.com>
2018-10-18 23:29:32 +0200

    Initialize

---
+++ global.R
@@ -0,0 +1,38 @@
+#############################################
+#                                           #
+#               APP SENDIS                  #
+#  Author: F.Michel-Sendis                  #
+#                                           #
+#############################################
+library(DT)
+library(data.table)
+library(dplyr)
+library(stringr)
+library(plotly)
+library(ggplot2)
+library(splitstackshape)
+
+
+#############################################
+#                                           #
+#  DATA INPUT, PREP DATA                    #
+#                                           #
+#############################################
+
+#read data:
+  df_calcs<-fread('data/mydata.csv')
+  df_calcs$V1<-NULL
+  df_calcs[df_calcs==""]<-NA
+#merging with expdata
+  expdata<-fread('data/all_expdata.csv')
+  expdata$V1<-NULL
+  df<-merge(expdata, df_calcs, all=FALSE)
+#RE-CREATE missing columns
+  df<- df %>% mutate(
+    COVERE=CALCVAL/EXPVAL,
+    RESIDUAL = (CALCVAL-EXPVAL)/sqrt(EXPERR^2 + CALCERR^2))
+  df$LIBVERA<-df$LIBVER
+  df<-cSplit(as.data.table(df), "LIBVERA", "-")
+  setnames(df, old=c("LIBVERA_1","LIBVERA_2"), new=c("LIB", "VER"))
+#tidy up
+  df<-df%>%arrange(INST, LIBVER, CASETYPE)
---
+++ myplot.R
@@ -0,0 +1,19 @@
+myplot <- function(df, inst){
+#select and prep data
+  df1<-df%>%
+    filter(INST==inst, FISS!="SPEC")%>%
+    mutate(TOTERR=sqrt(CALCERR^2 + EXPERR^2))
+#prep plot
+  g<-ggplot() +
+    geom_ribbon(data=df1, aes(x= EALF, ymin = 1-EXPERR, ymax= 1+EXPERR), alpha=0.2) +
+    geom_point(data=df1, aes(x=EALF, y=COVERE, colour=LIBVER, shape=FISS), alpha=0.8) +
+    labs(x = "Average Neutron Energy", y = "C/E")+
+    ylim(0.95, 1.03) +
+    scale_x_log10()+ annotation_logticks()+
+    theme_bw()+ theme(legend.title = element_blank())
+#print the plot to a pdf file
+    pdf("myplot.pdf")
+    print(g)
+    dev.off()
+return()
+ }
