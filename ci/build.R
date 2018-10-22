a816e02f8aa391cd0412a6becc29764b8ddf851f
fmichelsendis <franco.michelsendis@gmail.com>
2018-10-22 23:37:20 +0200

    update

--- ci/build.R
+++ ci/build.R
@@ -1,3 +1,12 @@
-source('global.R')
-source('myplot.R')
-myplot(df)
+source('../app/global.R')
+source('../libs/myfunctions.R')
+
+library(testthat)
+library(shinytest)
+
+test_that("Application works", {
+  # Use compareImages=FALSE because the expected image screenshots were created
+  # on a Mac, and they will differ from screenshots taken on the CI platform,
+  # which runs on Linux.
+  expect_pass(testApp("../app", compareImages = FALSE))
+})
