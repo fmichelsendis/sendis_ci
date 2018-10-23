930495a3cd4ce42b428a69613198d46e48dd06c0
fmichelsendis <franco.michelsendis@gmail.com>
2018-10-23 21:44:59 +0200

    update

--- tests/mytest.R
+++ tests/mytest.R
@@ -1,9 +1,6 @@
 app <- ShinyDriver$new("../")
 app$snapshotInit("mytest")
 
-# Input '`.clientValue-plotly_relayout-A`' was set, but doesn't have an input binding.
-# Input '`.clientValue-plotly_relayout-A`' was set, but doesn't have an input binding.
-app$setInputs(LIB3 = "JEFF-3.2")
-app$setInputs(LIB3 = c("JEFF-3.2", "JEFF-3.0"))
-app$setInputs(LIB3 = c("JEFF-3.2", "JEFF-3.0", "JEFF-3.3"))
-app$snapshot()
+app$setInputs(LIB3 = "JEFF-3.3")
+app$setInputs(LIB3 = c("JEFF-3.3", "ENDFB-VIII.b4"))
+app$snapshot(list(output = "plot_cumul"))
